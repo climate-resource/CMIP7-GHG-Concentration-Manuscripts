13f750ef509efc12ad43d0a8dfd440c50874d3d8
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2026-03-05 16:00:16 +1100

    Add placeholder

---
+++ notebooks/cci-2026-poster.py
@@ -0,0 +1,26 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.3
+#   kernelspec:
+#     display_name: ''
+#     name: ''
+# ---
+
+# %%
+- delivered historical and scenarios (one figure)
+    - link to user guides? (requires posting and making QR code)
+    - used in CMIP7 as boundary conditions
+    - global means, plus monthly products with seasonality plus lat gradient
+    - seasonality and lat gradient requires some modelling given we don't have global coverage
+    - leads to project
+- working on integrating satellite obs and surface measurements (one figure)
+    - satellites have high temporal and spatial coverage
+    - limited period of obs
+    - measuring a fundamentally different thing
+    - hence require algorithms to blend these sources of information (current focus of the project)
+- sketch layout in a miro board
