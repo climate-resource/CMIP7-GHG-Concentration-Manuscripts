848b89e9831c9e7fe7b190f383bd62b26e3a7aef
Zebedee Nicholls <zebedee.nicholls@climate-resource.com>
2026-09-23 13:50:14 +1000

    Write CO2 methods section

--- scripts/create-cmip7-historical-ghgs-manuscript.sh
+++ scripts/create-cmip7-historical-ghgs-manuscript.sh
@@ -127,6 +127,7 @@ uv run python "${script_dir}/compile-gmd-template-based-latex.py" \
     --section "${output_requirements_file}" \
     --section "${methods_file}" \
     --figure-file "<n2o-methods-figure>=${n2o_methods_figure_file}" \
+    --figure-file "<co2-methods-figure>=${co2_methods_figure_file}" \
     --section "${results_file}" \
     --conclusion "${conclusion_file}" \
     --code-and-data-availability "${code_and_data_availability_file}" \
